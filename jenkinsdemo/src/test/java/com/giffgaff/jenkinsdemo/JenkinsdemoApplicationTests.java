5d27133eadca56a80a23147d9799a90983d68bd1
RajkumarRepo <60997827+Raj2020Github@users.noreply.github.com>
2020-04-02 12:29:52 +0530

    JenkinsDemo

---
+++ jenkinsdemo/src/test/java/com/giffgaff/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.giffgaff.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.validation.constraints.AssertTrue;
+
+
+@SpringBootTest
+class JenkinsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+	@Test
+	void appTest(){
+
+	}
+
+}
